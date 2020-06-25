3bc0c73a13e8c0e2285a7ac6ad83fe78c8f6e88e
ibakun <ibakun@noltic.com>
2020-06-25 13:11:49 +0300

    feat: add timeout Add timeout with default time of 1 minute

--- src/commands/gin/sharing/waitReady.ts
+++ src/commands/gin/sharing/waitReady.ts
@@ -1,4 +1,6 @@
-import { SfdxCommand } from "@salesforce/command";
+import { flags, SfdxCommand } from "@salesforce/command";
+import { Duration } from "@salesforce/kit";
+import { SfdxError } from "@salesforce/core";
 
 export default class WaitReady extends SfdxCommand {
   public static description =
@@ -10,37 +12,79 @@ export default class WaitReady extends SfdxCommand {
     `,
   ];
 
-  protected static flagsConfig = {};
+  protected static flagsConfig = {
+    timeout: flags.minutes({
+      char: "t",
+      description: "During in minutes before command fails",
+      required: false,
+      default: Duration.minutes(1),
+    }),
+  };
   protected static requiresUsername = true;
   protected static requiresDevhubUsername = false;
   protected static requiresProject = false;
 
   public async run(): Promise<any> {
-
     this.ux.startSpinner("waiting for permission set groups to resolve");
     await this.waitAllPermissions();
-    this.ux.stopSpinner("\nall permission set groups were successfully updated");
-
+    this.ux.stopSpinner(
+      "\nall permission set groups were successfully updated"
+    );
   }
 
   private async waitAllPermissions() {
     const conn = this.org.getConnection();
 
-    const repeatTimeout = 2000;
-    const psGroupQuery = `SELECT Id,MasterLabel,Status, ManageableState, Description FROM PermissionSetGroup WHERE Status != 'Updated'`;
+    let totalTime = 0;
+    const repeatInterval = 2000;
+    const timeout = this.flags.timeout.quantity * 60 * 1000;
+    const psGroupQuery = `SELECT Id,MasterLabel,Status FROM PermissionSetGroup WHERE Status != 'Updated'`;
 
-    const isCompleted = () => new Promise((resolve, reject) => {
+    const isCompleted = () =>
+      new Promise((resolve, reject) => 
+        setTimeout(async () => {
 
-      setInterval(async () => {
-        const unfinishedPsGroups = await conn.tooling.query(psGroupQuery);
+          totalTime += repeatInterval;
+          const unfinishedPsGroups = await conn.tooling.query(psGroupQuery);
 
-        if (unfinishedPsGroups.totalSize > 0) {
-          this.ux.startSpinner(`Persmission Set Group left to resolve: ${unfinishedPsGroups.totalSize}`);
-        }
-        resolve(unfinishedPsGroups.totalSize === 0);
-      }, repeatTimeout)
-    });
+          if (unfinishedPsGroups.totalSize > 0) {
+            if (totalTime > timeout) {
+              reject(this.getTimeoutErrorMessage(unfinishedPsGroups.records));
+            } else {
+              this.ux.startSpinner(
+                `Persmission Set Group left to resolve: ${unfinishedPsGroups.totalSize}`
+              );
+            }
+          }
+          resolve(unfinishedPsGroups.totalSize === 0);
+        }, repeatInterval)
+      );
 
-    while(!(await isCompleted())) {}
+    try {
+      while (!(await isCompleted())) {}
+    } catch (e) {
+      throw new SfdxError(e);
+    }
+  }
+
+
+  getTimeoutErrorMessage(records) {
+    const stringifyRecords = (records) =>
+      records
+        .map((_: { Id; MasterLabel; Status }) =>
+          Object.assign({
+            Id: _.Id,
+            MasterLabel: _.MasterLabel,
+            Status: _.Status,
+          })
+        )
+        .map(JSON.stringify)
+        .join("\n");
+
+    return `
+Permission Set Group Update took too long: 
+Here is unfinished PersmissionSetGroups:
+${stringifyRecords(records)}
+        `;
   }
 }
