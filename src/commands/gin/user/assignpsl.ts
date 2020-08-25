fc6325f6f8bfdd62ce10a28c3eba4a2f075dea7f
Ivan Bakun <ibakun@Ivans-MacBook-Pro.local>
2020-08-25 18:11:28 +0300

    feature: add command for assigning CPQ license

---
+++ src/commands/gin/user/assignpsl.ts
@@ -0,0 +1,44 @@
+import { flags, SfdxCommand } from "@salesforce/command";
+import { SfdxError, fs } from "@salesforce/core";
+import { SandboxOrgConfig } from "@salesforce/core/lib/config/sandboxOrgConfig";
+
+export default class WaitReady extends SfdxCommand {
+    public static description =
+        `Inserts CPQ Permission Set License to Users`;
+
+    public static examples = [
+        `$ sfdx gin:user:assignpsl -u target_org" 
+    `,
+    ];
+
+    protected static flagsConfig = {
+        userfilter: flags.string({
+            char: 'f',
+            description: 'filter string for users',
+            required: false,
+            default: ''
+        })
+    };
+    protected static requiresUsername = true;
+    protected static requiresDevhubUsername = false;
+    protected static requiresProject = false;
+
+    public async run(): Promise<any> {
+        await this.insertPermissionSetGroups();
+    }
+
+    private async insertPermissionSetGroups() {
+        const permissionName = 'SalesforceCPQ_CPQStandardPerm';
+
+        const users = await conn.query(`SELECT Id, Name FROM User ${this.flags.userfilter ? 'WHERE ' + this.flags.userfilter: ''}`);
+        const permissionSetLicenses = await conn.query(`SELECT Id, DeveloperName FROM PermissionSetLicense WHERE DeveloperName = '${permissionName}'`);
+
+        const permissionSetAssignments = users.records.map(_ => Object.assign({
+                PermissionSetLicenseId: permissionSetLicenses.records[0].Id,
+                AssigneeId: _.Id
+            })
+        );
+
+        await conn.insert('PermissionSetLicense', permissionSetAssignments);
+    }
+}
